764417bb723c3a92fb98a214b0de605cb5c6d2df
Masahiro Tonogai <tonomasa36@gmail.com>
2021-06-05 19:42:12 +0900

    チェックワーク6月5日スクール途中

--- チェックワーク/ViewController.swift
+++ チェックワーク/ViewController.swift
@@ -14,6 +14,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label3: UILabel!
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
 
+    let foodArray: [String] = ["ハンバーグ","カレー","焼肉","お寿司"]
+    
+    var index: Int = 0
+    
+    @IBAction func random() {
+        let foodIndex = Int.random(in: 0...3)
+        
+        label2.text = foodArray[foodIndex]
+        
+        if index > 3 {
+            index = 0
+        }
+    }
+    
+    @IBAction func next(){
+        label2.text = foodArray[index]
+        
+        index = index + 1
+        
+        if index > 3 {
+            index = 0
+        }
+        
+    }
 }
 
